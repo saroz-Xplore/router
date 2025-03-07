7e7a33e8a1f62d283de7fca329bc1d1238e6f311
Saroj <sarojsimkhada2@gmail.com>
2025-03-07 21:44:23 +0545

    responsive page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
 import { FaSearch, FaShoppingCart } from "react-icons/fa";
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { GiHamburgerMenu } from "react-icons/gi";
+import { ImCross } from "react-icons/im";
 
 const Header = () => {
 
   const nav = useNavigate()
   const [scrolled, setScrolled] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false)
 
   const handleScroll = () => {
     const trackScrollValue = window.scrollY
@@ -14,21 +17,36 @@ const Header = () => {
 
   window.addEventListener('scroll', handleScroll)
 
+
   return (
-    <header className={`fixed top-0 ${scrolled ? "bg-{#051922]" : "bg-transparent"} w-[100vw] py-[15px] text-white`}>
-      <div className='mx-auto w-[1110px] flex items-center justify-between'>
+    <header className={`fixed top-0  ${scrolled ? "bg-[#051922]" : "bg-transparent"} w-[100vw] py-[15px] text-white`}>
+      <div className='mycontainer mx-auto   flex items-center justify-between'>
         <h1 className='text-[#F28123] text-[32px] font-bold cursor-pointer' onClick={() => nav('/')}>Fruitkha</h1>
 
-        <nav className='flex items-center gap-8 font-semibold'>
-          <NavLink to="/" className={({ isActive }) => `${isActive && "text-[#F28123] "}`}>Home</NavLink>
-          <NavLink to="/about" className={({ isActive }) => `${isActive && "text-[#F28123]"}`}>About Us</NavLink>
-          <Link>Shop</Link>
-          <Link>Contact Us</Link>
-        </nav>
+        <div className='hidden lg:block'>
+          <nav className='  flex items-center gap-8 font-semibold'>
+            <NavLink to="/" className={({ isActive }) => `${isActive && "text-[#F28123] "}`}>Home</NavLink>
+            <NavLink to="/about" className={({ isActive }) => `${isActive && "text-[#F28123]"}`}>About Us</NavLink>
+            <Link>Shop</Link>
+            <Link>Contact Us</Link>
+          </nav>
+        </div>
 
-        <div className='flex gap-6'>
-          <FaShoppingCart />
-          <FaSearch />
+        <div className='hidden lg:block'>
+          <div className='flex gap-6'>
+            <FaShoppingCart />
+            <FaSearch />
+          </div>
+        </div>
+
+        <div className='block lg:hidden text-3xl cursor-pointer' onClick={() => {
+          setMenuOpen((prev) => !prev)
+          console.log('stte', menuOpen)
+        }}>
+          {
+            menuOpen ? <ImCross /> : <GiHamburgerMenu />
+          }
+
         </div>
       </div>
     </header>
