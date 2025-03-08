15720ec18d9b7c95f5472cbe0c1b33e386fa665d
Saroj <sarojsimkhada2@gmail.com>
2025-03-08 21:18:13 +0545

    products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-export const Products = () => {
+const Products = () => {
   return (
-    <div>Products</div>
-  )
-}
+    <div className='bg-[#f5f5f5] py-[80px]'>
+      <div className='mycontainer text-center'>
+        <h1 className='text-4xl font-bold mb-6'>
+          <span className='text-[#F28123]'>Our</span>{' '}
+          <span className='text-[#051922]'>Products</span>
+        </h1>
+        <p className='text-[#555] text-lg max-w-[600px] mx-auto'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur nulla vitae, ab magni cupiditate ea.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Products
+export default Products;
